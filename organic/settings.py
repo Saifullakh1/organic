d9131044b26f0eea49f02249ecc67f674ec66ec0
Saifullakh1 <saifullakh7@gmail.com>
2024-03-11 10:03:34 +0600

    added postgresql

--- organic/settings.py
+++ organic/settings.py
@@ -85,6 +85,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': 'organic-db',
+#             'USER': 'postgres',
+#             'PASSWORD': '12345',
+#             'HOST': 'localhost',
+#             'PORT': 5432,
+#         }
+# }
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
